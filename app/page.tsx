a4d6fd16dbf956aac0a04d076d414d0990d18e3b
vtonu <victor.tonu@yahoo.com>
2024-10-08 15:38:31 -0700

    Footer removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import { SelectedCompanyProvider } from '@/hooks/useSelectedCompany';
 import { MainPanel } from '@/components/ui/mainPanel';
-import { FooterStuff } from '@/components/ui/footer';
 
 export default function Home() {
   return (
     <main className="h-[875px] flex justify-center items-center pt-4 px-2 flex-col">
       <SelectedCompanyProvider>
         <MainPanel />
-        {/* <FooterStuff /> */}
       </SelectedCompanyProvider>
     </main>
   );
