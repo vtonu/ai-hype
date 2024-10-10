8b67fd2ef32ee10dd150d2509fab8dfbbe941f5f
vtonu <victor.tonu@yahoo.com>
2024-10-10 08:12:26 -0700

    Fix linting issues

--- components/ui/main/Companybox.tsx
+++ components/ui/main/Companybox.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+// biome-ignore lint/style/useImportType: <explanation>
 import { AICompanyData } from "@/app/data/aiCompanyData";
 import { Button } from "@/components/ui/button";
 import {
@@ -42,6 +43,7 @@ export function AICompanySelector({
 			<PopoverTrigger asChild>
 				<Button
 					variant="outline"
+					// biome-ignore lint/a11y/useSemanticElements: <explanation>
 					role="combobox"
 					aria-expanded={open}
 					className="w-full justify-between"
