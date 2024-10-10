fac6897a6bb5419151f8a26d57ae2b3f1de9974a
vtonu <victor.tonu@yahoo.com>
2024-10-09 19:58:37 -0700

    use-toast.ts

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -95,6 +95,7 @@ export const reducer = (state: State, action: Action): State => {
 			if (toastId) {
 				addToRemoveQueue(toastId);
 			} else {
+				// biome-ignore lint/complexity/noForEach: <explanation>
 				state.toasts.forEach((toast) => {
 					addToRemoveQueue(toast.id);
 				});
@@ -132,9 +133,9 @@ let memoryState: State = { toasts: [] };
 
 function dispatch(action: Action) {
 	memoryState = reducer(memoryState, action);
-	listeners.forEach((listener) => {
+	for (const listener of listeners) {
 		listener(memoryState);
-	});
+	}
 }
 
 type Toast = Omit<ToasterToast, "id">;
@@ -179,7 +180,7 @@ function useToast() {
 				listeners.splice(index, 1);
 			}
 		};
-	}, [state]);
+	}, []);
 
 	return {
 		...state,
